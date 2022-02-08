aa9632de011bc11078a1db5868403f975d77f9f9
Nanxi <leenanxi@gmail.com>
2022-02-08 20:47:13 +0800

    Update Dockerfile.go

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,27 +1,30 @@
-# a protobuf just contains goalng for arm64 and x86
 ARG ALPINE_VERSION=3.15
 ARG GO_VERSION=1.17
-ARG ARCH=aarch_64
 ARG PROTOBUF_VERSION=3.19.4
 ARG PROTOC_GEN_GO_VERSION=v1.27.1
 ARG PROTOC_GEN_GO_GRPC_VERSION=v1.43.0
 ARG GRPC_GATEWAY_VERSION=v2.7.3
 ARG GOOGLEAPIS_VERSION=master
 ARG PROTOC_GEN_DOC_VERSION=v1.5.0
+ARG GLIBC_VERSION=2.34-r0
 
 FROM golang:${GO_VERSION}-alpine as builder
 RUN apk add --no-cache curl unzip
 
-RUN export ARCH=x86_64 && \
+ARG GLIBC_VERSION
+RUN mkdir -p /out/lib/ && export ARCH=x86_64 && \
     if [[ $(arch) == *"aarch64"* ]] ;then \
     export ARCH=aarch_64 && \
     curl -sSL https://releases.linaro.org/components/toolchain/binaries/latest-7/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-i686-mingw32_aarch64-linux-gnu.tar.xz -o /tmp/gcc-linaro.tar.xz && \
     cd /tmp/ && tar -xJvf gcc-linaro.tar.xz -C ./ --strip-components=3 gcc-linaro-7.5.0-2019.12-i686-mingw32_aarch64-linux-gnu/aarch64-linux-gnu/libc/lib &&  \
-    cp lib/ld-linux-aarch64.so.1 lib/libpthread.so.0 lib/libc.so.6 /out/lib/ \
-    ; fi
+    cp lib/ld-linux-aarch64.so.1 lib/libpthread.so.0 lib/libc.so.6 /out/lib/ ; else mkdir -p /out/tmp/ /out/etc/apk/keys/ &&  \
+    curl -sSL https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub -o /out/etc/apk/keys/sgerrand.rsa.pub && \
+    curl -sSL https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-${GLIBC_VERSION}.apk -o /out/tmp/glibc.apk; fi
 
 ARG PROTOBUF_VERSION
-RUN mkdir -p /out/usr/ && curl -sSL https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-linux-${ARCH}.zip -o /tmp/protobuf.zip  && \
+RUN mkdir -p /out/usr/ &&  \
+    export ARCH=x86_64 && if [[ $(arch) == *"aarch64"* ]] ;then export ARCH=aarch_64; fi && \
+    curl -sSL https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-linux-${ARCH}.zip -o /tmp/protobuf.zip  && \
     unzip -o /tmp/protobuf.zip -d /out/usr/ && \
     rm /out/usr/readme.txt
 
@@ -77,6 +80,10 @@ RUN mkdir -p ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
 
 FROM alpine:${ALPINE_VERSION}
 COPY --from=builder /out/ /
+
+RUN if [[ $(arch) == *"x86_64"* ]] ;then \
+    apk add --no-cache /tmp/glibc.apk && \
+    rm /etc/apk/keys/sgerrand.rsa.pub /tmp/glibc.apk ; fi \
 
 RUN mkdir -p /build/proto/third_party /build/go/third_party /build/openapi /build/java
 
