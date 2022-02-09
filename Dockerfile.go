4f5b6b45959901d4206c6dee0fc5bdf72dc786d4
Nanxi <leenanxi@gmail.com>
2022-02-09 10:44:50 +0800

    Update Dockerfile.go

--- Dockerfile.go
+++ Dockerfile.go
@@ -7,6 +7,7 @@ ARG GRPC_GATEWAY_VERSION=v2.7.3
 ARG GOOGLEAPIS_VERSION=master
 ARG PROTOC_GEN_DOC_VERSION=v1.5.0
 ARG GLIBC_VERSION=2.34-r0
+ARG PROTOC_GEN_VALIDATE_VERSION=v0.6.3
 
 FROM golang:${GO_VERSION}-alpine as builder
 RUN apk add --no-cache curl unzip
@@ -32,32 +33,27 @@ ARG PROTOC_GEN_GO_VERSION
 RUN mkdir -p ${GOPATH}/src/google.golang.org/protobuf && \
     curl -sSL https://github.com/protocolbuffers/protobuf-go/archive/refs/tags/${PROTOC_GEN_GO_VERSION}.tar.gz | tar xz --strip 1 -C ${GOPATH}/src/google.golang.org/protobuf &&\
     cd ${GOPATH}/src/google.golang.org/protobuf && \
-    CGO_ENABLED=0 go build -ldflags '-w -s' -o /golang-protobuf-out/protoc-gen-go ./cmd/protoc-gen-go && \
-    install -Ds /golang-protobuf-out/protoc-gen-go /out/usr/bin/protoc-gen-go
+    CGO_ENABLED=0 go build -ldflags '-w -s' -o /out/usr/bin/protoc-gen-go ./cmd/protoc-gen-go
 
 ARG PROTOC_GEN_GO_GRPC_VERSION
 RUN mkdir -p ${GOPATH}/src/github.com/grpc/grpc-go && \
     curl -sSL https://api.github.com/repos/grpc/grpc-go/tarball/${PROTOC_GEN_GO_GRPC_VERSION} | tar xz --strip 1 -C ${GOPATH}/src/github.com/grpc/grpc-go &&\
     cd ${GOPATH}/src/github.com/grpc/grpc-go/cmd/protoc-gen-go-grpc && \
-    CGO_ENABLED=0 go build -ldflags '-w -s' -o /golang-protobuf-out/protoc-gen-go-grpc && \
-    install -Ds /golang-protobuf-out/protoc-gen-go-grpc /out/usr/bin/protoc-gen-go-grpc
+    CGO_ENABLED=0 go build -ldflags '-w -s' -o /out/usr/bin/protoc-gen-go-grpc
 
 ARG PROTOC_GEN_VALIDATE_VERSION
 RUN mkdir -p ${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate && \
     curl -sSL https://api.github.com/repos/envoyproxy/protoc-gen-validate/tarball/${PROTOC_GEN_VALIDATE_VERSION} | tar xz --strip 1 -C ${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate && \
     cd ${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate && \
-    CGO_ENABLED=0 go build -ldflags '-w -s' -o /protoc-gen-validate-out/protoc-gen-validate . && \
-    install -Ds /protoc-gen-validate-out/protoc-gen-validate /out/usr/bin/protoc-gen-validate && \
+    CGO_ENABLED=0 go build -ldflags '-w -s' -o  /out/usr/bin/protoc-gen-validate . && \
     install -D ./validate/validate.proto /out/usr/include/github.com/envoyproxy/protoc-gen-validate/validate/validate.proto
 
 ARG GRPC_GATEWAY_VERSION
 RUN mkdir -p ${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway && \
     curl -sSL https://api.github.com/repos/grpc-ecosystem/grpc-gateway/tarball/${GRPC_GATEWAY_VERSION} | tar xz --strip 1 -C ${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway && \
     cd ${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway && \
-    CGO_ENABLED=0 go build -ldflags '-w -s' -o /grpc-gateway-out/protoc-gen-grpc-gateway ./protoc-gen-grpc-gateway && \
-    CGO_ENABLED=0 go build -ldflags '-w -s' -o /grpc-gateway-out/protoc-gen-openapiv2 ./protoc-gen-openapiv2 && \
-    install -Ds /grpc-gateway-out/protoc-gen-grpc-gateway /out/usr/bin/protoc-gen-grpc-gateway && \
-    install -Ds /grpc-gateway-out/protoc-gen-openapiv2 /out/usr/bin/protoc-gen-openapiv2 && \
+    CGO_ENABLED=0 go build -ldflags '-w -s' -o /out/usr/bin/protoc-gen-grpc-gateway ./protoc-gen-grpc-gateway && \
+    CGO_ENABLED=0 go build -ldflags '-w -s' -o  /out/usr/bin/protoc-gen-openapiv2 ./protoc-gen-openapiv2 && \
     mkdir -p /out/usr/include/protoc-gen-openapiv2/options && \
     install -D $(find ./protoc-gen-openapiv2/options -name '*.proto') -t /out/usr/include/protoc-gen-openapiv2/options
 
@@ -75,15 +71,14 @@ ARG PROTOC_GEN_DOC_VERSION
 RUN mkdir -p ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
     curl -sSL https://api.github.com/repos/pseudomuto/protoc-gen-doc/tarball/${PROTOC_GEN_DOC_VERSION} | tar xz --strip 1 -C ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
     cd ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
-    CGO_ENABLED=0 go build -ldflags '-w -s' -o /protoc-gen-doc-out/protoc-gen-doc ./cmd/protoc-gen-doc && \
-    install -Ds /protoc-gen-doc-out/protoc-gen-doc /out/usr/bin/protoc-gen-doc
+    CGO_ENABLED=0 go build -ldflags '-w -s' -o /out/usr/bin/protoc-gen-doc ./cmd/protoc-gen-doc
 
 FROM alpine:${ALPINE_VERSION}
 COPY --from=builder /out/ /
 
 RUN if [[ $(arch) == *"x86_64"* ]] ;then \
     apk add --no-cache /tmp/glibc.apk && \
-    rm /etc/apk/keys/sgerrand.rsa.pub /tmp/glibc.apk ; fi \
+    rm /etc/apk/keys/sgerrand.rsa.pub /tmp/glibc.apk ; fi
 
 RUN mkdir -p /build/proto/third_party /build/go/third_party /build/openapi /build/java
 
@@ -112,7 +107,7 @@ RUN echo $'if ! [ -d ./third_party ]; then return 0; fi && find ./third_party -t
 RUN echo $'find ./ -not -path "./third_party/*" -type f -name '*.proto' -exec protoc -I . --proto_path=/usr/include \
  --go_out /build/go --go_opt paths=source_relative \
  --go-grpc_out /build/go --go-grpc_opt paths=source_relative \
- --java_out=/build/java --grpc-java_out=/build/java \
+ --java_out=/build/java \
  --grpc-gateway_out /build/go --grpc-gateway_opt logtostderr=true \
  --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true \
  --validate_out=lang=go,paths=source_relative:/build/go \
