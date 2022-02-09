a5306158179d9bbcdfadeee313c2f23e7123d8ce
Nanxi <leenanxi@gmail.com>
2022-02-09 15:37:55 +0800

    Update Dockerfile.go

--- Dockerfile.go
+++ Dockerfile.go
@@ -3,9 +3,8 @@ ARG ALPINE_VERSION=3.15
 ARG GO_VERSION=1.17
 ARG PROTOBUF_VERSION=3.19.4
 ARG PROTOC_GEN_GO_VERSION=v1.27.1
-ARG PROTOC_GEN_GO_GRPC_VERSION=v1.43.0
+ARG PROTOC_GEN_GO_GRPC_VERSION=v1.44.0
 ARG GRPC_GATEWAY_VERSION=v2.7.3
-ARG GOOGLEAPIS_VERSION=master
 ARG PROTOC_GEN_DOC_VERSION=v1.5.0
 ARG GLIBC_VERSION=2.34-r0
 ARG PROTOC_GEN_VALIDATE_VERSION=v0.6.3
@@ -38,29 +37,28 @@ RUN mkdir -p ${GOPATH}/src/google.golang.org/protobuf && \
 
 ARG PROTOC_GEN_GO_GRPC_VERSION
 RUN mkdir -p ${GOPATH}/src/github.com/grpc/grpc-go && \
-    curl -sSL https://api.github.com/repos/grpc/grpc-go/tarball/${PROTOC_GEN_GO_GRPC_VERSION} | tar xz --strip 1 -C ${GOPATH}/src/github.com/grpc/grpc-go &&\
+    curl -sSL https://github.com/grpc/grpc-go/archive/refs/tags/${PROTOC_GEN_GO_GRPC_VERSION}.tar.gz | tar xz --strip 1 -C ${GOPATH}/src/github.com/grpc/grpc-go &&\
     cd ${GOPATH}/src/github.com/grpc/grpc-go/cmd/protoc-gen-go-grpc && \
     CGO_ENABLED=0 go build -ldflags '-w -s' -o /out/usr/bin/protoc-gen-go-grpc
 
 ARG PROTOC_GEN_VALIDATE_VERSION
 RUN mkdir -p ${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate && \
-    curl -sSL https://api.github.com/repos/envoyproxy/protoc-gen-validate/tarball/${PROTOC_GEN_VALIDATE_VERSION} | tar xz --strip 1 -C ${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate && \
+    curl -sSL https://github.com/envoyproxy/protoc-gen-validate/archive/refs/tags/${PROTOC_GEN_VALIDATE_VERSION}.tar.gz | tar xz --strip 1 -C ${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate && \
     cd ${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate && \
     CGO_ENABLED=0 go build -ldflags '-w -s' -o  /out/usr/bin/protoc-gen-validate . && \
     install -D ./validate/validate.proto /out/usr/include/github.com/envoyproxy/protoc-gen-validate/validate/validate.proto
 
 ARG GRPC_GATEWAY_VERSION
 RUN mkdir -p ${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway && \
-    curl -sSL https://api.github.com/repos/grpc-ecosystem/grpc-gateway/tarball/${GRPC_GATEWAY_VERSION} | tar xz --strip 1 -C ${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway && \
+    curl -sSL https://github.com/grpc-ecosystem/grpc-gateway/archive/refs/tags/${GRPC_GATEWAY_VERSION}.tar.gz | tar xz --strip 1 -C ${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway && \
     cd ${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway && \
     CGO_ENABLED=0 go build -ldflags '-w -s' -o /out/usr/bin/protoc-gen-grpc-gateway ./protoc-gen-grpc-gateway && \
     CGO_ENABLED=0 go build -ldflags '-w -s' -o  /out/usr/bin/protoc-gen-openapiv2 ./protoc-gen-openapiv2 && \
     mkdir -p /out/usr/include/protoc-gen-openapiv2/options && \
     install -D $(find ./protoc-gen-openapiv2/options -name '*.proto') -t /out/usr/include/protoc-gen-openapiv2/options
 
-ARG GOOGLEAPIS_VERSION
 RUN mkdir -p ${GOPATH}/src/github.com/googleapis/googleapis && \
-    curl -sSL https://api.github.com/repos/googleapis/googleapis/tarball/${GOOGLEAPIS_VERSION} | tar xz --strip 1 -C ${GOPATH}/src/github.com/googleapis/googleapis && \
+    curl -sSL https://github.com/googleapis/googleapis/archive/refs/heads/master.tar.gz | tar xz --strip 1 -C ${GOPATH}/src/github.com/googleapis/googleapis && \
     cd ${GOPATH}/src/github.com/googleapis/googleapis && \
     mkdir -p /out/usr/include/google/api && \
     cp -r ./google/api/*.proto /out/usr/include/google/api/ && \
@@ -70,7 +68,7 @@ RUN mkdir -p ${GOPATH}/src/github.com/googleapis/googleapis && \
 
 ARG PROTOC_GEN_DOC_VERSION
 RUN mkdir -p ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
-    curl -sSL https://api.github.com/repos/pseudomuto/protoc-gen-doc/tarball/${PROTOC_GEN_DOC_VERSION} | tar xz --strip 1 -C ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
+    curl -sSL https://github.com/pseudomuto/protoc-gen-doc/archive/refs/tags/${PROTOC_GEN_DOC_VERSION}.tar.gz | tar xz --strip 1 -C ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
     cd ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
     CGO_ENABLED=0 go build -ldflags '-w -s' -o /out/usr/bin/protoc-gen-doc ./cmd/protoc-gen-doc
 
@@ -100,20 +98,20 @@ service YourService { \n\
     } \n\
 }' >> /build/proto/main.proto
 
-RUN echo $'if ! [ -d ./third_party ]; then return 0; fi && find ./third_party -type f -name '*.proto' -exec protoc -I ./third_party --proto_path=/usr/include \
+RUN echo $'#!/bin/sh\nif ! [ -d ./third_party ]; then return 0; fi && find ./third_party -type f -name '*.proto' -exec protoc -I ./third_party --proto_path=/usr/include \
  --go_out /build/go/third_party --go_opt paths=source_relative \
  --go-grpc_out /build/go/third_party --go-grpc_opt paths=source_relative \
  {} \;' >> /build/build_third_party.sh
 
-RUN echo $'find ./ -not -path "./third_party/*" -type f -name '*.proto' -exec protoc -I . --proto_path=/usr/include \
+RUN echo $'#!/bin/sh\nfind ./ -not -path "./third_party/*" -type f -name '*.proto' -exec protoc -I . --proto_path=/usr/include \
  --go_out /build/go --go_opt paths=source_relative \
  --go-grpc_out /build/go --go-grpc_opt paths=source_relative \
- --java_out=/build/java \
  --grpc-gateway_out /build/go --grpc-gateway_opt logtostderr=true \
  --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true \
  --validate_out=lang=go,paths=source_relative:/build/go \
- --openapiv2_out /build/openapi --openapiv2_opt json_names_for_fields=false --openapiv2_opt \
-logtostderr=true {} \;' >> /build/build.sh
+ --openapiv2_out /build/openapi --openapiv2_opt json_names_for_fields=false --openapiv2_opt logtostderr=true \
+ --java_out=/build/java \
+ {} \;' >> /build/build.sh
 
 RUN chmod +x /build/build.sh /build/build_third_party.sh
 RUN chown -R nobody.nobody /build /usr/include
