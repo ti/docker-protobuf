6ebf636af2ca0f74be2f50b78631333ae1bd581a
Nanxi <leenanxi@gmail.com>
2022-08-03 17:03:58 +0800

    upgrade protoc versions with alpine edge

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,34 +1,16 @@
 # a simple protoc tool that Compatible with both arm and x86
 # docker buildx build --push --platform linux/arm64,linux/amd64 --tag nanxi/protoc:go .
-ARG ALPINE_VERSION=3.16
-ARG GO_VERSION=1.18
-ARG PROTOBUF_VERSION=3.20.1
-ARG PROTOC_GEN_GO_VERSION=v1.28.0
+ARG ALPINE_VERSION=edge
+ARG GO_VERSION=1.19
+ARG PROTOBUF_VERSION=21.4
+ARG PROTOC_GEN_GO_VERSION=v1.28.1
 ARG PROTOC_GEN_GO_GRPC_VERSION=v1.48.0
-ARG GRPC_GATEWAY_VERSION=v2.11.0
+ARG GRPC_GATEWAY_VERSION=v2.11.1
 ARG PROTOC_GEN_DOC_VERSION=v1.5.1
-ARG GLIBC_VERSION=2.35-r0
 ARG PROTOC_GEN_VALIDATE_VERSION=v0.6.7
 
 FROM golang:${GO_VERSION}-alpine as builder
 RUN apk add --no-cache curl unzip
-
-ARG GLIBC_VERSION
-RUN mkdir -p /out/lib/ && export ARCH=x86_64 && \
-    if [[ $(arch) == *"aarch64"* ]] ;then \
-    export ARCH=aarch_64 && \
-    curl -sSL https://releases.linaro.org/components/toolchain/binaries/latest-7/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-i686-mingw32_aarch64-linux-gnu.tar.xz -o /tmp/gcc-linaro.tar.xz && \
-    cd /tmp/ && tar -xJvf gcc-linaro.tar.xz -C ./ --strip-components=3 gcc-linaro-7.5.0-2019.12-i686-mingw32_aarch64-linux-gnu/aarch64-linux-gnu/libc/lib &&  \
-    cp lib/ld-linux-aarch64.so.1 lib/libpthread.so.0 lib/libc.so.6 /out/lib/ ; else mkdir -p /out/tmp/ /out/etc/apk/keys/ &&  \
-    curl -sSL https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub -o /out/etc/apk/keys/sgerrand.rsa.pub && \
-    curl -sSL https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-${GLIBC_VERSION}.apk -o /out/tmp/glibc.apk; fi
-
-ARG PROTOBUF_VERSION
-RUN mkdir -p /out/usr/ &&  \
-    export ARCH=x86_64 && if [[ $(arch) == *"aarch64"* ]] ;then export ARCH=aarch_64; fi && \
-    curl -sSL https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-linux-${ARCH}.zip -o /tmp/protobuf.zip  && \
-    unzip -o /tmp/protobuf.zip -d /out/usr/ && \
-    rm /out/usr/readme.txt
 
 ARG PROTOC_GEN_GO_VERSION
 RUN mkdir -p ${GOPATH}/src/google.golang.org/protobuf && \
@@ -58,6 +40,20 @@ RUN mkdir -p ${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway && \
     mkdir -p /out/usr/include/protoc-gen-openapiv2/options && \
     install -D $(find ./protoc-gen-openapiv2/options -name '*.proto') -t /out/usr/include/protoc-gen-openapiv2/options
 
+ARG PROTOC_GEN_DOC_VERSION
+RUN mkdir -p ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
+    curl -sSL https://github.com/pseudomuto/protoc-gen-doc/archive/refs/tags/${PROTOC_GEN_DOC_VERSION}.tar.gz | tar xz --strip 1 -C ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
+    cd ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
+    CGO_ENABLED=0 go build -ldflags '-w -s' -o /out/usr/bin/protoc-gen-doc ./cmd/protoc-gen-doc
+
+ARG PROTOBUF_VERSION
+RUN mkdir -p ${GOPATH}/src/github.com/protocolbuffers/protobuf &&  \
+    export ARCH=x86_64 && if [[ $(arch) == *"aarch64"* ]] ;then export ARCH=aarch_64; fi && \
+    curl -sSL https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-linux-${ARCH}.zip -o /tmp/protobuf.zip  && \
+    unzip -o /tmp/protobuf.zip -d  ${GOPATH}/src/github.com/protocolbuffers/protobuf && \
+    cd ${GOPATH}/src/github.com/protocolbuffers/protobuf && \
+    cp -r ./include /out/usr/
+
 RUN mkdir -p ${GOPATH}/src/github.com/googleapis/googleapis && \
     curl -sSL https://github.com/googleapis/googleapis/archive/refs/heads/master.tar.gz | tar xz --strip 1 -C ${GOPATH}/src/github.com/googleapis/googleapis && \
     cd ${GOPATH}/src/github.com/googleapis/googleapis && \
@@ -67,18 +63,11 @@ RUN mkdir -p ${GOPATH}/src/github.com/googleapis/googleapis && \
     cp -r ./google/rpc/*.proto /out/usr/include/google/rpc/ && \
     cp -r ./google/rpc/context/*.proto /out/usr/include/google/rpc/context/
 
-ARG PROTOC_GEN_DOC_VERSION
-RUN mkdir -p ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
-    curl -sSL https://github.com/pseudomuto/protoc-gen-doc/archive/refs/tags/${PROTOC_GEN_DOC_VERSION}.tar.gz | tar xz --strip 1 -C ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
-    cd ${GOPATH}/src/github.com/pseudomuto/protoc-gen-doc && \
-    CGO_ENABLED=0 go build -ldflags '-w -s' -o /out/usr/bin/protoc-gen-doc ./cmd/protoc-gen-doc
 
+ARG ALPINE_VERSION
 FROM alpine:${ALPINE_VERSION}
 COPY --from=builder /out/ /
-
-RUN if [[ $(arch) == *"x86_64"* ]] ;then \
-    apk add --no-cache /tmp/glibc.apk && \
-    rm /etc/apk/keys/sgerrand.rsa.pub /tmp/glibc.apk ; fi
+RUN apk add --no-cache protoc
 
 RUN mkdir -p /build/proto/third_party /build/go/third_party /build/openapi /build/java /build/docs
 
